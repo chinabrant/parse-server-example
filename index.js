5954c89304846b2daf1c3a6fac120ae3c6fc77ac
brant(吴述军) <812662025@qq.com>
2022-01-24 18:56:05 +0800

    feat: 添加文件支持

--- index.js
+++ index.js
@@ -13,15 +13,24 @@ const databaseUri = "mongodb://localhost/test"
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
+
+var FSFilesAdapter = require('@parse/fs-files-adapter');
+
+var fsAdapter = new FSFilesAdapter({
+  "filesSubDirectory": "~/files", // optional, defaults to ./files
+  "encryptionKey": "filewu0815" //optional, but mandatory if you want to encrypt files
+});
+
 const config = {
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || 'xx', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
-  },
+  filesAdapter: fsAdapter,
+  // liveQuery: {
+  //   classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
+  // },
 };
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -40,8 +49,8 @@ var dashboard = new ParseDashboard({
   ],
   users: [
     {
-      user: "",
-      pass: ""
+      user: "brant",
+      pass: "123456"
     }
   ]
 }, true);
